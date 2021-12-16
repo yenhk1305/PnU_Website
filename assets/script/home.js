c719f312fb47f9887361f7d8fdd7ce66aa2ac039
Hồ Kim Ngân <91685108+kimngan26301uel@users.noreply.github.com>
2021-12-16 16:41:37 +0700

    home push

---
+++ assets/script/home.js
@@ -0,0 +1,25 @@
+
+function plusSlides() {
+    showSlides(slideIndex += 1);
+}
+function minusSlides() {
+    showSlides(slideIndex -= 1);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length - 1) { slideIndex = 0 }
+    if (n < 0) { slideIndex = slides.length - 1 }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+}
