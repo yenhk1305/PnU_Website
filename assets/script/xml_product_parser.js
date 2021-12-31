6bd39f4f8952bd2c188eed4daea1554dc00b58b1
yenhk1305 <92587395+yenhk1305@users.noreply.github.com>
2021-12-31 23:31:06 +0700

    fix bug web browser

--- assets/script/xml_product_parser.js
+++ assets/script/xml_product_parser.js
@@ -1,76 +1,55 @@
 
-
 var data = [], xmlObject,xmlDoc;
 
-  
-function loadingProductPage1() {
-    
-    var xmlObject = new XMLHttpRequest();
-    xmlObject.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            data = changeXmlToData(this)
-        }
-    };
-    xmlObject.open("GET", "../data/product.xml", true);
-    xmlObject.send();
-
-    
-    function changeXmlToData(xml) {
-
-        xmlDoc = xml.responseXML;
-
-        var lstproduct,product;
-        var  name,price, srcImg;
-        var nameArray, priceArray, imgArray;
-
-        lstproduct = xmlDoc.getElementsByTagName('PnU')[0].getElementsByTagName("product");
-        
-        // Trộn mảng để thể hiện ngẫu nhiên
-        const lstproductShuffled = Array.prototype.slice.call(lstproduct).sort( function () {
-            return 0.5 - Math.random();
-        } );
- 
-        
-
-        var flag= document.getElementById("page2");
-
-        var btnCart=document.getElementsByClassName("product-item__add-cart");
-        nameArray = document.getElementsByClassName("product-item__name");
-        priceArray = document.getElementsByClassName("product-item__price-current");
-        imgArray = document.getElementsByClassName("product-item__img");
-        
-        if (flag==null){
-          
-           
-            for (i = 0; i < 20; i++) {
-
-                btnCart[i].setAttribute("onclick","addtocart(this)");
-                product =   lstproductShuffled[i];
-                name =product.getElementsByTagName("name")[0].childNodes[0].nodeValue;
-                nameArray [i].innerText = name;          
-            
-                price =product.getElementsByTagName("price")[0].childNodes[0].nodeValue;
-                priceArray [i].innerText = price;   
-                
-                srcImg =product.getElementsByTagName("image")[0].childNodes[0].nodeValue;
-                imgArray [i].src = srcImg;   
-            }
-        }else{
-            for (j = 20; j < 40; j++) {
-        
-                product =   lstproductShuffled[j];
-                name =product.getElementsByTagName("name")[0].childNodes[0].nodeValue;
-                nameArray [j-20].innerText = name;          
-            
-                price=product.getElementsByTagName("price")[0].childNodes[0].nodeValue;
-                priceArray [j-20].innerText = price;   
-                
-                srcImg =product.getElementsByTagName("image")[0].childNodes[0].nodeValue;
-                imgArray [j-20].src = srcImg;   
-            }
-            return data;
-        }
-
-       return data;
+if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
     }
+    else {// code for IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET","../data/product.xml" , false);
+    xmlhttp.send();
+    xmlDoc = xmlhttp.responseXML;
+
+    var  slsp=20;
+    var  name,price, srcImg;
+    var nameArray, priceArray, imgArray;
+    var btnCart=document.getElementsByClassName("product-item__add-cart");
+
+    nameArray = document.getElementsByClassName("product-item__name");
+    priceArray = document.getElementsByClassName("product-item__price-current");
+    imgArray = document.getElementsByClassName("product-item__img");
+
+
+function loadingProductOnPage(numberPage) {
+     
+    var lstproduct = xmlDoc.getElementsByTagName('PnU')[0].getElementsByTagName("product");
+
+       // Trộn mảng để thể hiện ngẫu nhiên
+    var   lstproductShuffled = Array.prototype.slice.call(lstproduct).sort( function () {
+        return -8.2;
+    } );
+
+
+    for (i = 0+20*numberPage; i < 20*(numberPage+1); i++) {
+
+        product =   lstproductShuffled[i];
+        name= product.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+        price =product.getElementsByTagName("price")[0].childNodes[0].nodeValue;
+        srcImg =product.getElementsByTagName("image")[0].childNodes[0].nodeValue;
+        
+        nameArray [i-slsp*numberPage].innerText = name;                   
+        priceArray [i-slsp*numberPage].innerText = price;      
+        imgArray [i-slsp*numberPage].src = srcImg;   
+    }
+  
 }
+
+function loadingProductPage1() {
+    loadingProductOnPage(0);
+}
+
+function loadingProductPage2() {
+    loadingProductOnPage(1);
+}
+
