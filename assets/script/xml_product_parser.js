94995f32240e67d54b7a403bea0a90f538b98d69
yenhk1305 <92587395+yenhk1305@users.noreply.github.com>
2022-01-02 17:10:56 +0700

    reponsive PC14, Tablet, Mobile

--- assets/script/xml_product_parser.js
+++ assets/script/xml_product_parser.js
@@ -1,7 +1,7 @@
 
 var data = [], xmlObject,xmlDoc;
 
-if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
     xmlhttp = new XMLHttpRequest();
     }
     else {// code for IE6, IE5
@@ -11,6 +11,7 @@ if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
     xmlhttp.send();
     xmlDoc = xmlhttp.responseXML;
 
+    // số sản phẩm trên một trang
     var  slsp=20;
     var  name,price, srcImg;
     var nameArray, priceArray, imgArray;
@@ -20,28 +21,28 @@ if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
     priceArray = document.getElementsByClassName("product-item__price-current");
     imgArray = document.getElementsByClassName("product-item__img");
 
-
 function loadingProductOnPage(numberPage) {
+
+    var n=numberPage;
      
     var lstproduct = xmlDoc.getElementsByTagName('PnU')[0].getElementsByTagName("product");
 
-       // Trộn mảng để thể hiện ngẫu nhiên
+       // Sắp xếp mảng để thể hiện ngẫu nhiên
     var   lstproductShuffled = Array.prototype.slice.call(lstproduct).sort( function () {
-        return -8.2;
+        return 0;
     } );
 
 
-    for (i = 0+20*numberPage; i < 20*(numberPage+1); i++) {
-
-        btnCart[i].setAttribute("onclick","addtocart(this)");
+    for (i = 0+20*n; i < 20*(n+1); i++) {
         product =   lstproductShuffled[i];
         name= product.getElementsByTagName("name")[0].childNodes[0].nodeValue;
         price =product.getElementsByTagName("price")[0].childNodes[0].nodeValue;
         srcImg =product.getElementsByTagName("image")[0].childNodes[0].nodeValue;
         
-        nameArray [i-slsp*numberPage].innerText = name;                   
-        priceArray [i-slsp*numberPage].innerText = price;      
-        imgArray [i-slsp*numberPage].src = srcImg;   
+        nameArray [i-slsp*n].innerText = name;                   
+        priceArray [i-slsp*n].innerText = Intl.NumberFormat().format(price) + " đ";      
+        imgArray [i-slsp*n].src = srcImg;   
+        btnCart[i-slsp*n].setAttribute("onclick","addtocart(this)");
     }
   
 }
@@ -54,3 +55,52 @@ function loadingProductPage2() {
     loadingProductOnPage(1);
 }
 
+function parseXml(xml, arrayTags) {
+    let dom = null;
+    if (window.DOMParser) dom = (new DOMParser()).parseFromString(xml, "text/xml");
+    else if (window.ActiveXObject) {
+        dom = new ActiveXObject('Microsoft.XMLDOM');
+        dom.async = false;
+        if (!dom.loadXML(xml)) throw dom.parseError.reason + " " + dom.parseError.srcText;
+    }
+    else throw new Error("cannot parse xml string!");
+
+    function parseNode(xmlNode, result) {
+        if (xmlNode.nodeName == "#text") {
+            let v = xmlNode.nodeValue;
+            if (v.trim()) result['#text'] = v;
+            return;
+        }
+
+        let jsonNode = {},
+            existing = result[xmlNode.nodeName];
+        if (existing) {
+            if (!Array.isArray(existing)) result[xmlNode.nodeName] = [existing, jsonNode];
+            else result[xmlNode.nodeName].push(jsonNode);
+        }
+        else {
+            if (arrayTags && arrayTags.indexOf(xmlNode.nodeName) != -1) result[xmlNode.nodeName] = [jsonNode];
+            else result[xmlNode.nodeName] = jsonNode;
+        }
+
+        if (xmlNode.attributes) for (let attribute of xmlNode.attributes) jsonNode[attribute.nodeName] = attribute.nodeValue;
+
+        for (let node of xmlNode.childNodes) parseNode(node, jsonNode);
+    }
+
+    let result = {};
+    for (let node of dom.childNodes) parseNode(node, result);
+
+    return result;
+}
+
+
+function sortDes(){
+    const obj = parser.toJson(xmlDoc, { object: true });
+    var product = obj[0].root[0].product[0].nodeValue;
+
+
+    nameArray [i-slsp*numberPage].innerText = name;                   
+        priceArray [i-slsp*numberPage].innerText = price;      
+        imgArray [i-slsp*numberPage].src = srcImg;   
+}
